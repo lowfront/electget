be7771dc7d0c77e3936910be7216244feb039d99
lowfront <fronterior@hotmail.com>
2022-03-20 05:31:19 +0900

    init

---
+++ README.md
@@ -0,0 +1,108 @@
+# Electget
+
+`Electron` tools for creating Windows widgets that are fixed to desktop. 
+
+This package provides features for widget creation of your Electron application.
+
+- Prevent `BrowserWindow` from being hidden in ShowDesktop.
+- Prevent `BrowserWindow` from being minimized.
+- Prevent `BrowserWindow` from being hidden in AeroPeek.
+- Prevent changes in the `BrowserWindow` order.
+- Move `BrowserWindow` to the bottom of the windows.
+
+## Install
+This package uses `ffi-napi`, so you must meet the [ffi-napi requirements](https://github.com/node-ffi-napi/node-ffi-napi#installation) before installation.
+
+`npm i electget`
+
+## Usage
+Electget is available in the Electron main process.
+
+```js
+import electget from 'electget';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    title: 'Electget Test',
+    height: 600,
+    width: 800
+  });
+
+  loadWindow(win);
+
+  // Prevent BrowserWindow from being hidden in AeroPeek.
+  electget.preventFromAeroPeek(win);
+
+  // Prevent BrowserWindow from being hidden in ShowDesktop and minimized.
+  electget.preventFromShowDesktop(win);
+
+  // Prevent changes in the BrowserWindow order.
+  electget.preventChangeZOrder(win);
+
+  // Move BrowserWindow to the bottom of the windows
+  electget.moveToBottom(win);
+
+  return win;
+}
+```
+
+### Methods
+#### `alwaysOnBottom(browserWindow)`
+- `win` BrowserWindow - Target BrowserWindow object.
+
+`BrowserWindow` is fixed to the bottom without being minimized.
+Apply `moveToBottom`, `preventChangeZOrder`, `preventFromShowDesktop` at once. 
+
+#### `cancelAlwaysOnBottom(browserWindow)`
+- `win` BrowserWindow - Target BrowserWindow object.
+
+Cancel of `alwaysOnBottom` method.
+
+#### `preventFromAeroPeek(win)`
+- `win` Buffer|BrowserWindow - Target BrowserWindow object or Buffer returned by getNativeWindowHandle.
+
+Prevent `BrowserWindow` from being hidden in AeroPeek.
+
+#### `preventFromShowDesktop(win)`
+- `win` Buffer|BrowserWindow - Target BrowserWindow object or Buffer returned by getNativeWindowHandle.
+
+Prevent `BrowserWindow` from being hidden in ShowDesktop and minimized.
+
+#### `cancelPreventFromShowDesktop(win)`
+- `win` Buffer|BrowserWindow - Target BrowserWindow object or Buffer returned by getNativeWindowHandle.
+
+Cancel of `preventFromShowDesktop` method.
+
+#### `preventChangeZOrder(browserWindow)`
+- `win` BrowserWindow - Target BrowserWindow object.
+
+Returns `function` - Cancel of `preventChangeZOrder` method.
+
+Prevent changes in the `BrowserWindow` order.
+
+#### `moveToBottom(win)`
+- `win` Buffer|BrowserWindow - Target BrowserWindow object or Buffer returned by getNativeWindowHandle.
+
+Move `BrowserWindow` to the bottom of the windows.
+
+
+## Guidelines for Using Webpack
+Because `Webpack` bundles the source code, the following error occurs when `Webpack` is used for the main process: `Error: No native build was found ...`
+
+You can install the [webpack-node-externals](https://www.npmjs.com/package/webpack-node-externals) package and remove the node package from the bundle with the `Webpack` externals setting as [follows](https://github.com/node-ffi-napi/node-ffi-napi/issues/157#issuecomment-846426023):
+
+```js
+// webpack.config.js
+const nodeExternals = require('webpack-node-externals');
+module.exports = {
+  ...
+  externals: [nodeExternals()],
+  ...
+}
+```
+
+## Roadmap
+
+- [ ] Method failure check.
+- [ ] Add MacOS support.
+- [ ] Add test code.
---
+++ src/index.ts
@@ -0,0 +1,150 @@
+import ref from 'ref-napi';
+import ffi from 'ffi-napi';
+import os from 'os';
+import di from 'ref-struct-di';
+import {BrowserWindow} from 'electron';
+
+export const DWMWA_EXCLUDED_FROM_PEEK = 12;
+
+const StructType = di(ref);
+
+export const WINDOWPOS = StructType({
+  hwnd: ref.types.int32,
+  hwndInsertAfter: ref.types.int32,
+  x: ref.types.int32,
+  y: ref.types.int32,
+  cx: ref.types.int32,
+  cy: ref.types.int32,
+  flags: ref.types.uint32,
+});
+
+export const SWP_NOZORDER = 4;
+export const WM_WINDOWPOSCHANGING = 70;
+
+export const HWND_BOTTOM = 1;
+export const SWP_NOMOVE = 2;
+export const SWP_NOSIZE = 1;
+
+export type Win = BrowserWindow|Buffer;
+
+
+export function readBufferByOS(buf: Buffer) {
+  return os.endianness() == 'LE' ? buf.readInt32LE() : buf.readInt32BE();
+};
+
+export function getHWnd(win: Win) {
+  if (win instanceof BrowserWindow) return readBufferByOS(win.getNativeWindowHandle());
+  else if (win instanceof Buffer) return readBufferByOS(win);
+  else throw new Error('Type that doesn\'t support.');
+};
+
+const electget = new class Electget {
+  platform: NodeJS.Platform;
+  user32?: {
+    FindWindowExA: ffi.ForeignFunction<string | number, [string | number, string | number, string | null, string | number]>
+    GetDesktopWindow: ffi.ForeignFunction<string | number, []>;
+    SetWindowLongPtrA:  ffi.ForeignFunction<string | number, [number, number, number]>;
+    SetWindowPos: ffi.ForeignFunction<boolean, [string | number, string | number, number, number, number, number, number]>;
+  };
+  dwmapi?: {
+    DwmSetWindowAttribute: ffi.ForeignFunction<string | number, [string | number, string | number, ref.Pointer<boolean>, string | number]>;
+  };
+  kernel32?: {
+    GetLastError: ffi.ForeignFunction<string | number, []>;
+  };
+  constructor() {
+    this.platform = process.platform;
+
+    if (this.platform !== 'win32') {
+      console.warn('It does not support OS other than Windows.');
+    } else {
+      this.user32 = new ffi.Library('user32', {
+        FindWindowExA: ['ulong', ['ulong', 'ulong', 'string', 'ulong']],
+        GetDesktopWindow: ['ulong', []],
+        SetWindowLongPtrA: ['ulong', ['int', 'int', 'int']],
+        SetWindowPos: ['bool', ['ulong', 'ulong', 'int', 'int', 'int', 'int', 'uint']],
+      });
+      this.dwmapi = new ffi.Library('dwmapi.dll', {
+        DwmSetWindowAttribute: ['ulong', ['long', 'ulong', 'bool*', 'ulong']],
+      });
+      this.kernel32 = new ffi.Library('kernel32.dll', {
+        GetLastError: ['ulong', []],
+      });
+      
+    }
+  }
+
+  preventFromAeroPeek(win: Win) {
+    if (!this.dwmapi) return false;
+    const hWnd = getHWnd(win);
+    const bool = ref.alloc('bool', true);
+    this.dwmapi.DwmSetWindowAttribute(hWnd, DWMWA_EXCLUDED_FROM_PEEK, bool.ref(), ref.sizeof.int32);
+    return true;
+  }
+
+  preventFromShowDesktop(win: Win) {
+    if (!this.user32) return false;
+    const hWnd = getHWnd(win);
+    const progman = this.user32?.FindWindowExA(ref.NULL as any, ref.NULL as any, 'Progman', ref.NULL as any);
+    let defView = this.user32?.FindWindowExA(progman, ref.NULL as any, 'SHELLDLL_DefView', ref.NULL as any); 
+    
+    if (!defView) {// find again
+      const desktopHWnd = this.user32?.GetDesktopWindow() ?? 0;
+      let workerW = 0;
+      do {
+        workerW = this.user32?.FindWindowExA(desktopHWnd, workerW, 'WorkerW', ref.NULL as any) as any; 
+        defView = this.user32?.FindWindowExA(workerW, ref.NULL as any, 'SHELLDLL_DefView', ref.NULL as any);
+      } while (!defView && workerW);
+    }
+  
+    if (!defView) return false;
+  
+    this.user32?.SetWindowLongPtrA(hWnd, -8, defView as any);
+  
+    return true;
+  }
+
+  cancelPreventFromShowDesktop(win: Win) {
+    if (!this.user32) return false;
+    const hWnd = getHWnd(win);
+    this.user32.SetWindowLongPtrA(hWnd, -8, this.user32.GetDesktopWindow() as any);
+  }
+
+  preventChangeZOrder(browserWindow: BrowserWindow) {
+    browserWindow.hookWindowMessage(WM_WINDOWPOSCHANGING, (wParam, lParam)=> {
+      const buf = Buffer.alloc(8);
+      buf.type = ref.refType(WINDOWPOS);
+      lParam.copy(buf);
+      const actualStructDataBuffer = buf.deref();
+      const windowPos = actualStructDataBuffer.deref();
+      
+      const newFlags = windowPos.flags | SWP_NOZORDER;
+      actualStructDataBuffer.writeUInt32LE(newFlags, 6);
+    });
+  
+    return function cancelPreventChangeZOrder() {
+      browserWindow.unhookWindowMessage(WM_WINDOWPOSCHANGING);
+    };
+  }
+
+  moveToBottom(win: Win) {
+    if (!this.user32) return false;
+    const hWnd = getHWnd(win);
+    this.user32.SetWindowPos(hWnd, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOMOVE|SWP_NOSIZE);
+  }
+
+  alwaysOnBottom(browserWindow: BrowserWindow) {
+    const hWnd = browserWindow.getNativeWindowHandle();
+    this.moveToBottom(hWnd);
+    this.preventChangeZOrder(browserWindow);
+    this.preventFromShowDesktop(hWnd);
+  }
+
+  cancelAlwaysOnBottom(browserWindow: BrowserWindow) {
+    const hWnd = browserWindow.getNativeWindowHandle();
+    browserWindow.unhookWindowMessage(WM_WINDOWPOSCHANGING);
+    this.cancelPreventFromShowDesktop(hWnd);
+  }
+}
+
+export default electget;
